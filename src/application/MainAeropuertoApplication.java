200531864272de90bb0ecbd98395363fc9182741
Urtzi <urcrmedev@gmail.com>
2023-11-03 13:09:05 +0100

    UPDATE MainAeropuertoApplication to check for the login.

--- src/application/MainAeropuertoApplication.java
+++ src/application/MainAeropuertoApplication.java
@@ -10,23 +10,47 @@ import javafx.stage.Stage;
 
 public class MainAeropuertoApplication extends Application {
 
-    @Override
-    public void start(Stage stage) {
+	@Override
+	public void start(Stage stage) {
 		try {
-			Parent root = FXMLLoader.load(this.getClass().getResource("/fxml/listadoAeropuertos.fxml"));
-			
-	    	Scene scene = new Scene( root );
-	    	scene.getStylesheets().add(this.getClass().getResource("/css/main.css").toExternalForm());
+			Parent root = FXMLLoader.load(this.getClass().getResource("/fxml/loginAeropuertos.fxml"));
+			Scene loginScene = new Scene(root);
+			loginScene.getStylesheets().add(this.getClass().getResource("/css/main.css").toExternalForm());
+			stage.setTitle("Aviones - Login");
+			stage.setScene(loginScene);
+			stage.setResizable(false);
+			stage.show();
+			stage.setOnCloseRequest(e -> {
+				e.consume();
+				if (stage.getProperties().get("goodLogin") != null) {					
+					try {
+						
+						Parent mainRoot = FXMLLoader.load(this.getClass().getResource("/fxml/listadoAeropuertos.fxml"));
+						Scene mainScene = new Scene(mainRoot);
+						mainScene.getStylesheets().add(this.getClass().getResource("/css/main.css").toExternalForm());
+						Stage mainStage = new Stage();
+						
+						mainStage.setTitle("Aviones - Aeropuertos");
+						mainStage.setScene(mainScene);
+						mainStage.setResizable(false);
+						mainStage.show();
+						stage.close();
+					} catch (IOException e1) {
+						// TODO Auto-generated catch block
+						e1.printStackTrace();
+					}
+				} else {
+					stage.close();
+				}
 
-	        stage.setTitle("PERSONAS");
-	        stage.setScene(scene);
-	        stage.setResizable(false);
-	        stage.show();
+			});
+
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
-    }
-    public static void main(String[] args) {
+	}
+
+	public static void main(String[] args) {
 		launch(args);
 	}
 }
